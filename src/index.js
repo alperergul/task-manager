73372c7831b8dd376c73189ec68b483e18f068d3
alperergul <alper.ergul.97@gmail.com>
2024-04-25 23:32:29 +0300

    middleware added

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use((req, res, next) => {
-  res.status(503).send('Site is currently down. Check back soon!');
-});
+// app.use((req, res, next) => {
+//   res.status(503).send('Site is currently down. Check back soon!');
+// });
 
 app.use(express.json());
 app.use(userRouter);
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require('../models/user');
+const auth = require('../middleware/auth');
 const router = new express.Router();
 
 router.post('/users', async (req, res) => {
@@ -31,13 +32,8 @@ router.post('/users/login', async (req, res) => {
   }
 });
 
-router.get('/users', async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (error) {
-    res.status(500).send(error);
-  }
+router.get('/users/me', auth, async (req, res) => {
+  res.send(req.user);
 });
 
 router.get('/users/:id', async (req, res) => {
