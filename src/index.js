ebe1b8475482a31e42f8460606044854c3354f6c
alperergul <alper.ergul.97@gmail.com>
2024-04-29 21:55:24 +0300

    logout and hiding private data

--- src/index.js
+++ src/index.js
@@ -19,16 +19,3 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log('Server is up on port ' + port);
 });
-
-const jwt = require('jsonwebtoken');
-
-const myFunction = async () => {
-  const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', {
-    expiresIn: '7 days',
-  });
-
-  const data = jwt.verify(token, 'thisismynewcourse');
-  console.log(data);
-};
-
-myFunction();
--- src/models/user.js
+++ src/models/user.js
@@ -52,6 +52,17 @@ const userSchema = new mongoose.Schema({
   ],
 });
 
+userSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
+
+  delete userObject.password;
+  delete userObject.tokens;
+  console.log(userObject);
+
+  return userObject;
+};
+
 userSchema.methods.generateAuthToken = async function () {
   const user = this;
   const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse');
